7d65b0c124d70ac0a975a5eba334140510918a6b
Marcel Roth <MarcelRoth100@aol.com>
2024-06-05 14:24:53 +0200

    add slurm.sh

---
+++ train_effb3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH -p ls6
+#SBATCH -J train_ac
+#SBATCH --gres=gpu:rtx2080ti:1
+#SBATCH -o logs/slurm-%j.out
+
+python3 train_classification_model.py --data_dir data --model_name efficientnet_b3
