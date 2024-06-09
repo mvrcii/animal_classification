3c24913448d483b10ec63dc2f63e7e23bf8697f1
Marcel Roth <MarcelRoth100@aol.com>
2024-06-09 16:38:54 +0200

    update

--- train_cv.sh
+++ train_cv.sh
@@ -4,4 +4,4 @@
 #SBATCH --gres=gpu:rtx3090:1
 #SBATCH -o logs/slurm-%j.out
 
-python3 cross_validation.py --data_dir data --CV_fold_path data/cross_folds --model_name efficientnet_b3
+python3 cross_validation.py --data_dir data --CV_fold_path data/cross_folds --model_name efficientnet_b3 --batch_size 64
